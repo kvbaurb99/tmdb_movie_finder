15e2282f86bfb94a1c2cc2714e785a5cf1f086b9
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-05-05 09:49:32 +0200

    menu + mobile menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
+import genres from '../data/category.json'
+
+export default function Menu({ showGenres, setShowGenres}) {
+
+    const preventPropagation = (e) => {
+        e.stopPropagation();
+    }
+
+  return (
+    <div className='flex items-center gap-1 cursor-pointer relative'>
+        <li onClick={() => setShowGenres(!showGenres)} className='cursor-pointer hover:text-white/70 duration-500'>Gatunki</li>
+        <MdOutlineKeyboardArrowDown className='text-lg' />
+        <div onClick={preventPropagation} className={`absolute top-[120%] left-0 h-[160px] bg-black border border-white/50 z-80 lg:w-[200px] xl:w-[220px] 2xl:w-[250px] rounded cursor-default text-white flex flex-wrap justify-start flex-col gap-1 p-2 lg:text-xs xl:text-sm ${showGenres ? 'flex' : 'hidden'}`}>
+            {
+                genres.map(genre => (
+                    <li className='cursor-pointer hover:underline py-1'>{genre.title}</li>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import genres from '../data/category.json'
+import { AiOutlineClose } from 'react-icons/ai'
+import Link from 'next/link'
+
+export default function MobileMenu({ showMobileMenu, setShowMobileMenu }) {
+  return (
+    <ul className={`absolute top-0 text-white left-0 lg:hidden flex-col ${showMobileMenu ? 'flex' : 'hidden'} w-full h-[500px] bg-black items-center gap-6 text-2xl font-bold justify-center`}>
+        <li>Strona Główna</li>
+        <Link href='https://www.themoviedb.org/movie' target='_blank'><li>Filmy</li></Link>
+        <Link href='https://www.themoviedb.org/tv' target='_blank'><li>Seriale</li></Link>
+        <div className='flex text-xs flex-wrap justify-center gap-2'>
+            {
+                genres.map(genre => (
+                <div className='bg-blue-600 rounded-full px-3 py-2'>
+                    {genre.title}
+                </div>
+                ))
+            }
+        </div>
+        <AiOutlineClose onClick={() => setShowMobileMenu(false)} className='absolute top-5 right-5' />
+    </ul>
+  )
+}
