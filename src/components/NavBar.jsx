02ade9f753d3c33969b366b3671fd8b1c89c4968
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-05-05 09:49:42 +0200

    Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
-import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
-import { FaMoon } from 'react-icons/fa'
+import { AiOutlineMenu } from 'react-icons/ai'
+import Menu from './Menu'
+import MobileMenu from './MobileMenu'
 
-export default function NavBar() {
+export default function NavBar({ showGenres, setShowGenres }) {
+
+    const [ showMobileMenu, setShowMobileMenu ] = useState(false)
+
   return (
     <nav className='w-full h-[100px] absolute z-50'>
         <div className='xl:w-[75%] mx-auto h-full flex text-white items-center justify-between'>
-            <div className='flex items-center'>
+            <div className='flex items-center lg:ml-[8rem] xl:ml-0'>
             <Image
                 src={Logo}
                 width={100}
                 alt='logo'
             />
             </div>
-            <ul className='flex gap-7 items-center'>
+            <ul className='gap-7 items-center lg:mr-[8rem] xl:mr-0 lg:flex hidden'>
                 <li className='cursor-pointer hover:text-white/70 duration-500'>Strona główna</li>
                 <Link href='https://www.themoviedb.org/movie' target='_blank'><li className='cursor-pointer hover:text-white/70 duration-500'>Filmy</li></Link>
                 <Link href='https://www.themoviedb.org/tv' target='_blank'><li className='cursor-pointer hover:text-white/70 duration-500'>Seriale</li></Link>
-                <div className='flex items-center gap-1 cursor-pointer relative'>
-                    <li className='cursor-pointer hover:text-white/70 duration-500'>Gatunki</li>
-                    <MdOutlineKeyboardArrowDown className='text-lg' />
-                </div>
+                <Menu setShowGenres={setShowGenres} showGenres={showGenres} />
             </ul>
+            <div onClick={() => setShowMobileMenu(true)} className='block lg:hidden mr-[2rem]'>
+                <AiOutlineMenu className='text-xl' />
+            </div>
         </div>
+        <MobileMenu showMobileMenu={showMobileMenu} setShowMobileMenu={setShowMobileMenu} />
     </nav>
   )
 }
